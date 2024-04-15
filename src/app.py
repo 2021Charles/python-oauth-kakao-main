bed18215bc57eab48df8f7727bcfa226c1281dd6
김창수 <charles1.kim@lge.com>
2024-04-15 13:32:52 +0900

    'added_1'

--- src/app.py
+++ src/app.py
@@ -13,6 +13,7 @@ from config import CLIENT_ID, REDIRECT_URI
 from controller import Oauth
 from model import UserModel, UserData
 
+#added 1
 
 app = Flask(__name__)
 app.config['JWT_SECRET_KEY'] = "I'M IML."
